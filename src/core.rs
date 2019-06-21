ed93a536d65b884afe61987508480f3264219e06
Dominik Braun <braun@sternentstehung.de>
2019-06-21 11:47:33 +0200

    built experimental struct scaffold

---
+++ src/core.rs
@@ -0,0 +1,22 @@
+#[derive(Debug)]
+pub struct App<'a, 'b> {
+    sc: &'a Scanner,
+    mg: &'b Manager,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Scanner {}
+
+impl Scanner {
+    fn run(&self, path: &str) {
+        unimplemented!();
+    }
+}
+
+#[derive(Debug)]
+struct Manager {
+    pool: Vec<Worker>,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Worker {}
