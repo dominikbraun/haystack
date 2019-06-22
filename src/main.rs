0e19da6e9d05f919035f45d661b68cbe97e9b543
aligator <aligator@suncraft-server.de>
2019-06-22 20:47:29 +0200

    if we use the term needle for the searchpattern, use haystack as directory path.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::io;
+
 use crate::core::Manager;
 use crate::core::Scanner;
 
-mod core; 
+mod core;
 mod app;
 
 fn main() -> Result<(), io::Error> {
     let matches = app::build().get_matches();
 
-    let dir = matches.value_of("dir").unwrap_or("./");
+    let dir = matches.value_of("haystack").unwrap_or("./");
     let term = matches.value_of("needle").unwrap();
 
     let haystack = Manager::new(term, 5);
