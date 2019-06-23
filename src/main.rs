e00794f16fd0448b49444e0581a57027c3685246
Dominik Braun <braun@sternentstehung.de>
2019-06-23 13:29:59 +0200

    outsourced code into run_stable()

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,17 @@ mod app;
 fn main() -> Result<(), io::Error> {
     let matches = app::build().get_matches();
 
-    let dir = matches.value_of("haystack").unwrap_or("./");
+    let dir = matches.value_of("haystack").unwrap();
     let term = matches.value_of("needle").unwrap();
 
     if matches.is_present("exp") {
         run_exp(dir, term)
+    } else {
+        run_stable(dir, term)
     }
+}
+
+fn run_stable(dir: &str, term: &str) -> Result<(), io::Error> {
     let haystack = Manager::new(term, 5)?;
     let _ = Scanner{}.run(&haystack, dir);
 
