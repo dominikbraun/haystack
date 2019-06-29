ec222b6a1ad3fcc2a4cd65e4bfaf8f488b5e4c30
Dominik Braun <braun@sternentstehung.de>
2019-06-29 22:05:43 +0200

    copied processing logic

--- src/exp.rs
+++ src/exp.rs
@@ -1,4 +1,7 @@
+use std::fs::File;
 use std::io;
+use std::io::{BufReader, Error, Read};
+use std::path::Path;
 use std::iter;
 use std::thread;
 
@@ -26,13 +29,26 @@ impl Manager {
     pub fn spawn(&self) {
         for _ in 0..self.pool_size {
             let stealer = self.queue.stealer();
+            let term = self.term.clone();
             let wg = self.wg.clone();
 
             thread::spawn(move || {
                 loop {
                     if let Steal::Success(f) = stealer.steal() {
                         if f.is_empty() { break; }
-                        // processing logic
+
+                        let handle = match File::open(Path::new(&f)) {
+                            Ok(f) => f,
+                            Err(err) => {
+                                println!("{}", err);
+                                continue;
+                            }
+                        };
+                        let occurences = process(&term, handle);
+
+                        if occurences > 0 {
+                            println!("Found in {}", f);
+                        }
                     }
                 }
                 drop(wg);
@@ -64,4 +80,42 @@ pub fn scan(dir: &str, mg: &Manager) -> Result<(), io::Error> {
         }
     }
     Result::Ok(())
+}
+
+fn process(term: &str, handle: File) -> usize {
+    let mut reader = BufReader::new(handle);
+    let mut buf = vec![0; 8000];
+
+    let mut cursor = 0;
+    let mut positives = 0;
+
+    let term = term.as_bytes();
+
+    loop {
+        if let Ok(size) = reader.read(&mut buf) {
+            if size == 0 {
+                break;
+            }
+
+            for i in 0..size {
+                if buf[i] == term[cursor] {
+                    cursor = cursor + 1;
+                } else if cursor > 0 {
+                    if buf[i] == term[0] {
+                        cursor = 1;
+                    } else {
+                        cursor = 0;
+                    }
+                }
+
+                if cursor == term.len() {
+                    positives += 1;
+                    cursor = 0;
+                }
+            }
+        } else {
+            break;
+        }
+    }
+    positives
 }
