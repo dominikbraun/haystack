cdb527f85d5021e0b3a191cab55af8136f80d88a
Dominik Braun <braun@sternentstehung.de>
2019-06-30 19:25:54 +0200

    synchronized experimental stage

--- src/exp.rs
+++ src/exp.rs
@@ -1,85 +1,85 @@
 extern crate crossbeam;
 extern crate walkdir;
 
-use std::fs::File;
+use std::fs;
 use std::io;
 use std::io::{BufReader, Error, Read};
-use std::iter;
 use std::path::Path;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicU16, Ordering};
 use std::thread;
 
-use crossbeam::deque::{Injector, Steal, Stealer, Worker};
-use crossbeam::sync::WaitGroup;
+use crossbeam::deque::Injector;
+use crossbeam::deque::Steal;
+use crossbeam::sync;
 use walkdir::WalkDir;
 
 pub struct Manager {
-    queue: Arc<Injector<String>>,
     term: String,
+    queue: Arc<Injector<String>>,
     pool_size: usize,
-    wg: WaitGroup,
+    gate: sync::WaitGroup,
     total: Arc<AtomicU16>,
 }
 
 impl Manager {
     pub fn new(term: &str, pool_size: usize) -> Manager {
         Manager {
-            queue: Arc::new(Injector::<String>::new()),
             term: term.to_owned(),
+            queue: Arc::new(Injector::<String>::new()),
             pool_size,
-            wg: WaitGroup::new(),
+            gate: sync::WaitGroup::new(),
             total: Arc::new(AtomicU16::new(0)),
         }
     }
 
-    pub fn spawn(&self) {
-        for i in 0..self.pool_size {
+    pub fn spawn(&self) -> bool {
+        for _ in 0..self.pool_size {
             let term = self.term.clone();
-            let wg = self.wg.clone();
-            let total = Arc::clone(&self.total);
             let queue = Arc::clone(&self.queue);
-            
+            let gate = self.gate.clone();
+            let total = Arc::clone(&self.total);
+
             thread::spawn(move || {
                 loop {
                     if let Steal::Success(f) = queue.steal() {
-                        if f.is_empty() { break; }
-
-                        let handle = match File::open(Path::new(&f)) {
-                            Ok(f) => f,
-                            Err(err) => {
-                                continue;
-                            }
+                        if f.is_empty() {
+                            break;
+                        }
+                        let path = Path::new(&f);
+                        
+                        let handle = match fs::File::open(path) {
+                            Ok(h) => h,
+                            Err(e) => { continue; },
                         };
-                        let occurences = process(&term, handle);
 
-                        if occurences > 0 {
+                        if process(&term, handle) > 0 {
                             let mut val = total.load(Ordering::Relaxed);
-                            val += 1;
-                            total.store(val, Ordering::Relaxed);
+                            total.store(val + 1, Ordering::Relaxed);
                         }
                     }
                 }
-                drop(wg);
+                drop(gate);
             });
         }
+        true
     }
 
     fn take(&self, file: String) {
         self.queue.push(file);
     }
-    
+
     pub fn stop(self) -> u16 {
         for _ in 0..self.pool_size {
             self.queue.push(String::new());
         }
-        self.wg.wait();
+        self.gate.wait();
         
         return self.total.load(Ordering::Relaxed);
     }
 }
 
-pub fn scan(dir: &str, mg: &Manager) -> Result<(), io::Error> {
+pub fn scan(dir: &str, manager: &Manager) -> Result<(), io::Error> {
     
     let items = WalkDir::new(dir.to_owned()).into_iter().filter_map(|i| {
         i.ok()
@@ -88,30 +88,30 @@ pub fn scan(dir: &str, mg: &Manager) -> Result<(), io::Error> {
     for i in items {
         if i.file_type().is_file() {
             let path = i.path().display().to_string();
-            mg.take(path);
+            manager.take(path);
         }
     }
     Result::Ok(())
 }
 
-fn process(term: &str, handle: File) -> u16 {
+fn process(term: &str, handle: fs::File) -> u16 {
     let mut reader = BufReader::new(handle);
     let mut buf = vec![0; 8000];
 
     let mut cursor = 0;
-    let mut positives = 0;
+    let mut found: u16 = 0;
 
     let term = term.as_bytes();
 
     loop {
-        if let Ok(size) = reader.read(&mut buf) {
-            if size == 0 {
+        if let Ok(len) = reader.read(&mut buf) {
+            if len == 0 {
                 break;
             }
-
-            for i in 0..size {
+            
+            for i in 0..len {
                 if buf[i] == term[cursor] {
-                    cursor = cursor + 1;
+                    cursor += 1;
                 } else if cursor > 0 {
                     if buf[i] == term[0] {
                         cursor = 1;
@@ -121,13 +121,13 @@ fn process(term: &str, handle: File) -> u16 {
                 }
 
                 if cursor == term.len() {
-                    positives += 1;
+                    found += 1;
                     cursor = 0;
                 }
             }
         } else {
-            break;
+            return 0;
         }
     }
-    positives
+    found
 }
